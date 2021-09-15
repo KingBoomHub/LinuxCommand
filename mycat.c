12d6338472c9a913bf714a3a3968d24af91eb124
KingBoomHub <907835790@qq.com>
2021-09-16 06:56:17 +0800

    添加while循环，实现多次输入回显功能

--- mycat.c
+++ mycat.c
@@ -1,6 +1,6 @@
 //功能：实现Linux下的cat功能
-//1.打开文件并输出内容到终端显示
-//2.回显输入的内容
+//1.回显输入的内容
+//2.打开文件并输出内容到终端显示
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -15,15 +15,20 @@ int main(int argc, char *argv[])
     int readcnt;
     char buff[4096];
     
+    //回显模式
     if (argc < 2) {
-        readcnt = read(STDIN_FILENO, buff, 10);
-        if (readcnt < 0) {
-            perror("read STDIN_FILENO");
-            exit(1);
+        while(1) 
+        {
+            readcnt = read(STDIN_FILENO, buff, 10);
+            if (readcnt < 0) {
+                perror("read STDIN_FILENO");
+                exit(1);
+            }
+            write(STDOUT_FILENO, buff, readcnt);
         }
-        write(STDOUT_FILENO, buff, readcnt);
     }
 
+    //文件显示模式
     fd = open(argv[1], O_RDONLY); //打开文件并设置只读模式，记录该文件的设备描述符	
     readcnt = read(fd, buff, sizeof(buff)); //根据文件设备描述符，读取文件数据到buff中，并统计数据长度
     write(1, buff, readcnt); //向屏幕写入buff中的数据
